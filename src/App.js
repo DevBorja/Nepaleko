26c2a1f4a4b12ac5f2f461d59753605830b46934
Borja Diez <devborja@gmail.com>
2024-09-22 11:00:50 +0200

    chore(proyect settings): :construction: Base style setUp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./assets/styles/App.css";
+import "beercss";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add(isDarkMode ? "dark" : "light");
+    document.body.classList.remove(isDarkMode ? "light" : "dark");
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav class="left">
+        <a>
+          <i>home</i>
+          <div>Home</div>
         </a>
-      </header>
+        <a>
+          <i>search</i>
+          <div>Search</div>
+        </a>
+        <a>
+          <i>share</i>
+          <div>share</div>
+        </a>
+      </nav>
+      <main class="responsive">
+        <h3>Compact</h3>
+        <article class="large-blur">
+          <h5>Card</h5>
+          <p>Some text here</p>
+          <nav class="right-align">
+            <button onClick={toggleTheme}>Tema</button>
+          </nav>
+        </article>
+      </main>
     </div>
   );
 }
